a8fd79d9dd06850e5a6beef9a3e6fedfa4a85f12
Shivtej Deshmukh <shivtejdeshmukh03@gmail.com>
2022-08-03 14:47:02 +0530

    String operations
    Concat and append

--- strings.cs
+++ strings.cs
@@ -1,17 +1,46 @@
 using System;
 using System.Text;
-namespace StringBuilders
+namespace StringOperations
 {
     class Strings
     {
         static void Main(string[] args)
         {
-            StringBuilder string1 =new StringBuilder(" Hello ", 50);
-            StringBuilder string2 = new StringBuilder(" World ", 50);
-            string1.Append(string2);
-            Console.WriteLine("The string after appending is {0}",string1.ToString());
+            string s1 = "My name is ";
+            string s2 = "Shivtej";
 
+            StringBuilder sbstring1 =new StringBuilder(" Hello ", 50);
+            StringBuilder sbstring2 = new StringBuilder(" World ", 50);
+            
+            Console.WriteLine("Concatinating two strings ");
+            StringOperations.StringConcat.concat(s1,s2);
+
+            Console.WriteLine("Appending two strings using string Builder");
+            StringOperations.StringAppend.sbappend(sbstring1,sbstring2);
+            
+            
+        }
+    }
+    class StringConcat
+    {
+        public static void concat(string s1,string s2)
+        {
+            string concatstring = "";
+            concatstring = s1+s2;
+            Console.WriteLine("The strings after using concat : " + concatstring+"\n");
+        }
+    }
+
+    class StringAppend
+    {
+        public static void sbappend(StringBuilder sb1,StringBuilder sb2)
+        {
+            StringBuilder sbstring = new StringBuilder("",50);
+            sbstring.Append(sb1);
+            sbstring.Append(sb2);
+            Console.WriteLine("The string after appending is : {0} \n",sbstring.ToString());
         }
     }
     
 }
+
